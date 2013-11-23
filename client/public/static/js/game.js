dded6b4acf5c4e0b71522e57f62546af119cab7a
Denis Yakunin <de.yakunin@gmail.com>
2013-11-23 16:18:04 +0100

    map: fields

--- client/public/static/js/game.js
+++ client/public/static/js/game.js
@@ -1,4 +1,18 @@
 $(function() {
+
+    for (var i = 0; i < 14; i++) {
+        var row = $('<div/>', {
+                'class': 'row'
+            }).appendTo('.map');
+        for (var j = 0; j < 14; j++) {
+            $('<div/>', {
+                'data-row': i,
+                'data-col': j,
+                'class': 'field'
+            }).appendTo(row);
+        }
+    };
+
     var socket = io.connect(":" + LocalConfig.port);
     socket.on("update", function (snapshot) {
         var myId = snapshot.myId;
