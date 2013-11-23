b2f932998c1514b7742dfe6294805ec1a36c860d
lenny <lennytmp@gmail.com>
2013-11-23 15:12:49 +0100

    bugs in world.js fixed

--- shared/config.js
+++ shared/config.js
@@ -1,2 +1,6 @@
 (function(exports){
+    exports.minWorldX = 0;
+    exports.minWorldY = 0;
+    exports.maxWorldX = 50;
+    exports.maxWorldY = 100;
 })(typeof exports === 'undefined'? this['Config']={}: exports);
