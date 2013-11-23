6f2a560306671121830cd86a1a4cc2a888aedbac
Denis Yakunin <de.yakunin@gmail.com>
2013-11-23 16:57:58 +0100

    refactoring

--- client/public/static/js/game.js
+++ client/public/static/js/game.js
@@ -13,11 +13,35 @@ $(function() {
         }
     };
 
+    var Game = function () {
+        this.id = null;
+        this.player = null;
+        this.phase = null;
+        this.units = null;
+    };
+
+    Game.prototype.update = function (snapshot) {
+        this.id = snapshot.myId;
+        this.player = snapshot.players[this.id];
+        this.phase = snapshot.world.phase;
+        this.units = [];
+        console.log(this.player);
+        this[snapshot.world.phase](snapshot);
+    };
+
+    Game.prototype.planning_phase = function (snapshot) {
+        console.log('planning');
+        var me = this;
+        setTimeout(function () {
+        }, 500);
+    };
+
     var socket = io.connect(":" + LocalConfig.port);
+    var game = new Game(socket);
+
     socket.on("update", function (snapshot) {
-        var myId = snapshot.myId;
-        var player = snapshot.players[myId];
         console.log(snapshot);
+        game.update(snapshot);
     });
 });
 
