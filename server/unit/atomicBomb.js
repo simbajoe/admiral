7dee6e69b41aeed14ad72811e012c7f8864a7445
lenny <lennytmp@gmail.com>
2013-11-24 19:14:59 +0100

    atomicBomb whereAttack added

--- server/unit/atomicBomb.js
+++ server/unit/atomicBomb.js
@@ -9,5 +9,15 @@ AtomicBomb.prototype = new Unit();
 AtomicBomb.prototype.setWhereAttack = function() {
     this.whereCanAttack = [];
     this.whereCouldAttack = [];
-    var shootingUnits = this.getUnitsCanShoot();
+    for (var x = this.location.x - 2; x <= this.location.x + 2; x++) {
+        for (var y = this.location.y - 2; y <= this.location.y + 2; y++) {
+            var cell = this.world.cells.get(x,y);
+            if (cell) {
+                this.whereCouldAttack.push(cell);
+                if (cell.hasEnemyObject()) {
+                    this.whereCouldAttack.push(cell);
+                }
+            }
+        }
+    }
 };
