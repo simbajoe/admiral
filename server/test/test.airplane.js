ce00020a2bd3456b4230e73ac4cfc8ac54386097
Denis Yakunin <de.yakunin@gmail.com>
2013-11-24 23:55:24 +0100

    merge

--- server/test/test.airplane.js
+++ server/test/test.airplane.js
@@ -1,9 +1,24 @@
 var World = require('../world');
 var Config = require('../../shared/config.js');
+var Fixture = require('./fixture.js');
 
 exports.testMobCreation = function(test) {
     var world = new World();
-    var airplane = world.addUnit(world.players[0], 'airplane', [2,2]);
-    test.ok(world.players[0].units.length > 0, 'Airplane was not added');
+    for (var p in Fixture.units) {
+        var i = 0;
+        for (var u in Fixture.units[p]) {
+            var unit = Fixture.units[p][u];
+            world.addUnit(world.players[p - 1], unit[0], unit[1]);
+            i++;
+            test.ok(
+                    world.players[p - 1].units.length == i,
+                    'Unit was not added: ' + JSON.stringify(unit)
+                    + '. Have units: ' + world.players[p - 1].units.length
+                    + '. Expected: ' + i
+                    + '. Player: ' + p + '.'
+                   );
+        }
+    }
     test.done();
 };
+
