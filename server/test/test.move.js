3fbfde4cee5038f771a74d52b9481b5075b40c22
Denis Yakunin <de.yakunin@gmail.com>
2013-11-28 19:02:26 +0100

    tests move

--- server/test/test.move.js
+++ server/test/test.move.js
@@ -163,3 +163,156 @@ exports.testMoveVedette = function(test) {
     test.done();
 };
 
+exports.testMoveMine01 = function(test) {
+
+    var map = [
+        "+-----------------------------------------------+",
+        "|   | 0  1  2  3  4  5  6  7  8  9  10 11 12 13 |",
+        "|---+-------------------------------------------|",
+        "|0  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|1  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|2  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|3  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|4  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|5  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|6  | .  .  .  .  .  .  .  1d 1m .  .  .  .  .  |",
+        "|7  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|8  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|9  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|10 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|11 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|12 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|13 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "+-----------------------------------------------+"
+    ];
+
+    var world = new World();
+
+    Util.setupWorld(test, world, map, world.players[0].id, Config.MOVE_PHASE);
+    Util.testWhereCanMove(test, world, [7, 6], [[7, 5], [7, 7], [6, 6]]);
+    Util.testWhereCanMove(test, world, [8, 6], [[8, 5], [8, 7]]);
+
+    test.done();
+};
+
+exports.testMoveMine02 = function(test) {
+
+    var map = [
+        "+-----------------------------------------------+",
+        "|   | 0  1  2  3  4  5  6  7  8  9  10 11 12 13 |",
+        "|---+-------------------------------------------|",
+        "|0  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|1  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|2  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|3  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|4  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|5  | .  .  .  .  .  .  .  .  1m .  .  .  .  .  |",
+        "|6  | .  .  .  .  .  .  .  1d .  .  .  .  .  .  |",
+        "|7  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|8  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|9  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|10 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|11 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|12 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|13 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "+-----------------------------------------------+"
+    ];
+
+    var world = new World();
+
+    Util.setupWorld(test, world, map, world.players[0].id, Config.MOVE_PHASE);
+    Util.testWhereCanMove(test, world, [7, 6], [[7, 5], [7, 7], [6, 6], [8, 6]]);
+    Util.testWhereCanMove(test, world, [8, 5], [[7, 5], [8, 6]]);
+
+    test.done();
+};
+
+exports.testMoveMine03 = function(test) {
+
+    var map = [
+        "+-----------------------------------------------+",
+        "|   | 0  1  2  3  4  5  6  7  8  9  10 11 12 13 |",
+        "|---+-------------------------------------------|",
+        "|0  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|1  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|2  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|3  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|4  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|5  | .  .  .  .  .  .  1m .  1m .  .  .  .  .  |",
+        "|6  | .  .  .  .  .  .  1m 1d .  .  .  .  .  .  |",
+        "|7  | .  .  .  .  .  .  1m .  .  .  .  .  .  .  |",
+        "|8  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|9  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|10 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|11 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|12 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|13 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "+-----------------------------------------------+"
+    ];
+
+    var world = new World();
+
+    Util.setupWorld(test, world, map, world.players[0].id, Config.MOVE_PHASE);
+    Util.testWhereCanMove(test, world, [7, 6], [[7, 5], [7, 7], [8, 6]]);
+    Util.testWhereCanMove(test, world, [8, 5], [[7, 5], [8, 6]]);
+    Util.testWhereCanMove(test, world, [6, 5], [[7, 5]]);
+    Util.testWhereCanMove(test, world, [6, 6], []);
+    Util.testWhereCanMove(test, world, [6, 7], [[7, 7]]);
+
+    test.done();
+};
+
+exports.testMoveCommon = function(test) {
+
+    var map = [
+        "+-----------------------------------------------+",
+        "|   | 0  1  2  3  4  5  6  7  8  9  10 11 12 13 |",
+        "|---+-------------------------------------------|",
+        "|0  | .  .  .  .  .  .  .  .  .  .  .  .  .  1p |",
+        "|1  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|2  | .  .  .  1B .  .  .  1aC.  .  1p .  .  .  |",
+        "|3  | .  .  1v .  .  .  .  .  .  .  .  .  .  .  |",
+        "|4  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|5  | .  1cS.  .  1r .  .  1fM.  .  1b .  .  .  |",
+        "|6  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|7  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|8  | .  1mS.  .  1fS.  .  1m .  .  1c .  .  .  |",
+        "|9  | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|10 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "|11 | .  1s .  .  1aB.  .  1a .  .  1t .  .  .  |",
+        "|12 | .  .  .  .  .  .  .  .  .  .  .  .  1v .  |",
+        "|13 | .  .  .  .  .  .  .  .  .  .  .  .  .  .  |",
+        "+-----------------------------------------------+"
+    ];
+
+    var world = new World();
+
+    Util.setupWorld(test, world, map, world.players[0].id, Config.MOVE_PHASE);
+    Util.testWhereCanMove(test, world, [2, 3], [[2, 1], [2, 2], [0, 3], [1, 3], [3, 3], [4, 3], [2, 4], [2, 5]]);
+    Util.testWhereCanMove(test, world, [3, 2], []);
+
+    Util.testWhereCanMove(test, world, [1, 5],  [[1, 4],  [1, 6],  [0, 5],  [2, 5]]);
+    Util.testWhereCanMove(test, world, [1, 8],  [[1, 7],  [1, 9],  [0, 8],  [2, 8]]);
+    Util.testWhereCanMove(test, world, [1, 11], [[1, 10], [1, 12], [0, 11], [2, 11]]);
+
+    Util.testWhereCanMove(test, world, [4, 5],  [[4, 4],  [4, 6],  [3, 5],  [5, 5]]);
+    Util.testWhereCanMove(test, world, [4, 8],  [[4, 7],  [4, 9],  [3, 8],  [5, 8]]);
+    Util.testWhereCanMove(test, world, [4, 11], [[4, 10], [4, 12], [3, 11], [5, 11]]);
+
+    Util.testWhereCanMove(test, world, [7, 2],  [[7, 1],  [7, 3],  [6, 2],  [8, 2]]);
+    Util.testWhereCanMove(test, world, [7, 5],  []);
+    Util.testWhereCanMove(test, world, [7, 8],  []);
+    Util.testWhereCanMove(test, world, [7, 11], []);
+
+    Util.testWhereCanMove(test, world, [10, 2],  [[10, 1],  [10, 3],  [9, 2],  [11, 2]]);
+    Util.testWhereCanMove(test, world, [10, 5],  [[10, 4],  [10, 6],  [9, 5],  [11, 5]]);
+    Util.testWhereCanMove(test, world, [10, 8],  [[10, 7],  [10, 9],  [9, 8],  [11, 8]]);
+    Util.testWhereCanMove(test, world, [10, 11], []);
+
+    Util.testWhereCanMove(test, world, [12, 12], [[12, 11], [12, 10], [12, 13], [10, 12], [11, 12], [13, 12]]);
+    Util.testWhereCanMove(test, world, [13, 0],  [[12, 0],  [13, 1]]);
+
+    test.done();
+};
+
+
